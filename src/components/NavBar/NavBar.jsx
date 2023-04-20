c1e27400afebed474a123114f0e78bffd9074362
Lourdes Debiaggi <ldebiaggi@hotmail.com>
2023-04-20 13:21:45 -0300

    Corrección render Navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,25 @@
-import CartWidget from "../CartWidget/CartWidget"
+import CartWidget from "../CartWidget/CartWidget";
+import { Navbar, Container, Nav } from 'react-bootstrap';
+
 
 const NavBar = () => {
     return(
-        <nav>
-            <h3>OA CatShop</h3>
-            <div>
-                <button>Alimentos y snacks</button>
-                <button>Juguetes</button>
-                <button>Cat Gyms</button>
-                <button>Accesorios</button>
-                <button>Higiene</button>
-            </div>
+        <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">OA CatShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#alimentos">Alimentos y snacks</Nav.Link>
+              <Nav.Link href="#juguetes">Juguetes</Nav.Link>
+              <Nav.Link href="#gyms">Cat Gyms</Nav.Link>
+              <Nav.Link href="#accesorios">Accesorios</Nav.Link>
+              <Nav.Link href="#higiene">Higiene</Nav.Link>
+            </Nav>
             <CartWidget />
-        </nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     )
 }
 
