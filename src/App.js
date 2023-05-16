46c58dc489f6e4ba3135d546f1ca231e24d96dab
Lourdes Debiaggi <ldebiaggi@hotmail.com>
2023-05-16 18:46:56 -0300

    use of context

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { CartProvider } from './context/CartContext';
 
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter> 
-        <NavBar />
+      <CartProvider>
+      <NavBar />
         <Routes>
           <Route path='/' element={<ItemListContainer />} />
           <Route path='/category/:categoryId' element={<ItemListContainer/>} />
           <Route path='/item/:itemId' element={ <ItemDetailContainer/> } />
           <Route path='*' element={<h1>404 NOT FOUND</h1>} />
         </Routes>
+      </CartProvider> 
       </BrowserRouter>
     </div>
   );
