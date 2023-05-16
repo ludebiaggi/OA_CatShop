811f7ced18cb4cdc29c8c021a62c24abbf2bd9c0
Lourdes Debiaggi <ldebiaggi@hotmail.com>
2023-05-16 18:25:19 -0300

    Counter Synchronization

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,7 +1,15 @@
 import "../../App.css"
+import { useState } from "react"
 import ItemCount from '../ItemCount/ItemCount'
+import { Link } from "react-router-dom"
 
 const ItemDetail =({id, name, img, category, description, price, stock}) => {
+    const [quantityAdded, setQuantityAdded] = useState(0)
+
+    const handleOnAdd =(quantity) => {
+        setQuantityAdded(quantity)
+    }
+
     return (
         <article className='CardItemDetail' >
             <header className='Header'>
@@ -18,7 +26,13 @@ const ItemDetail =({id, name, img, category, description, price, stock}) => {
                 <p className='Info'>Precio: ${price}</p>
             </section>
             <footer className='ItemFooter'>
-            <ItemCount initial={0} stock={10} onAdd={(quantity) =>console.log("Cantidad agregada", quantity)}/>
+                {
+                    quantityAdded > 0 ? (
+                        <Link to="/cart" className="Option"> Terminar Compra</Link>
+                    ) : (
+                        <ItemCount initial={0} stock={stock} onAdd={handleOnAdd}/>
+                    )
+                }
             </footer>
         </article>
 
